a9a37bc720fb8b14b021fde3466d2d5eb4676b78
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-06 15:23:49 -0400

    nivel2

--- Beecrowd.sql
+++ Beecrowd.sql
@@ -1,5 +1,7 @@
 --************Solucion de beecrowd Talent**********
--->>NIVEL 1
+
+				-->>NIVEL 1
+
 --Customer Address
 SELECT 
 	name,
@@ -16,6 +18,7 @@ FROM products;
 ---Expanding the Business
 SELECT DISTINCT city
 FROM customers;
+
 ---Provider Ajax SA
 SELECT
     products.name,
@@ -24,18 +27,135 @@ FROM products
 INNER JOIN providers
     ON products.id_providers = providers.id
 WHERE providers.name = 'Ajax SA';
+
 ---Legal Person
 SELECT customers.name
 FROM legal_person
 INNER JOIN customers
     ON legal_person.id_customers = customers.id;
+
 ---Passwords
-SELECT
-    id,
-    password,
-    MD5(password) as MD5
+SELECT id, password,MD5(password) 
 FROM account;
+
 ---Viruses
 SELECT REPLACE(name, 'H1', 'X')
 FROM virus;
+
+				-->>NIVEL 2
+
+---Under 10 or Greater Than 100
+SELECT id,name
+FROM products
+WHERE (price < 10) OR (price > 100);
+
+---Cheap Movies
+SELECT
+    movies.id,
+    movies.name
+FROM movies
+INNER JOIN prices
+    ON movies.id_prices = prices.id
+WHERE prices.value < 2;
+
+---Super Luxury
+SELECT products.name,categories.name
+FROM products
+INNER JOIN categories
+    ON products.id_categories = categories.id
+WHERE products.amount > 100
+    AND categories.id IN (1, 2, 3, 6, 9)
+ORDER BY categories.id ASC;
+
+---How much earn a Doctor?
+WITH c_hour_earns AS (VALUES(150))
+SELECT
+	doctors.name,
+	ROUND(SUM((attendances.hours * (table c_hour_earns)) 
+			  + (attendances.hours * (table c_hour_earns) * (work_shifts.bonus/100)))
+		 , 1) AS salary
+FROM doctors 
+INNER JOIN attendances
+	ON doctors.id = attendances.id_doctor
+INNER JOIN work_shifts
+	ON work_shifts.id = attendances.id_work_shift
+GROUP BY doctors.name
+ORDER BY salary DESC;
+
+---Sillas Adyacentes
+SELECT
+    c1.queue AS "queue",
+    c1.id AS "left",
+    c2.id AS "right"
+FROM
+    chairs c1
+    JOIN chairs c2 ON c1.queue = c2.queue AND c1.id + 1 = c2.id
+WHERE
+    c1.available = TRUE
+    AND c2.available = TRUE
+ORDER BY
+    c1.queue, c1.id;
+
+---Clasificación de un Árbol
+  SELECT
+    distinct node_id,
+    CASE
+      WHEN node_id = 50 THEN 'ROOT'
+      WHEN pointer IS NULL AND node_id IS NOT NULL THEN 'LEAF'
+      WHEN pointer IS NOT NULL AND node_id IS NOT NULL THEN 'INNER'
+    END AS type
+  FROM nodes
+)
+
+SELECT
+  node_id,
+  type
+FROM NodeTypes
+ORDER BY
+node_id;
+
+---Seguidores
+SELECT
+  LEAST(u1.user_name, u2.user_name) AS u1_name,
+  GREATEST(u1.user_name, u2.user_name) AS u2_name
+FROM
+  followers f
+  JOIN users u1 ON f.user_id_fk = u1.user_id
+  JOIN users u2 ON f.following_user_id_fk = u2.user_id
+WHERE
+  (u1.user_id, u2.user_id) IN (
+    SELECT
+      user_id_fk AS user_id1,
+      following_user_id_fk AS user_id2
+    FROM
+      followers
+    
+    UNION ALL
+    
+    SELECT
+      following_user_id_fk AS user_id1,
+      user_id_fk AS user_id2
+    FROM
+      followers
+  )
+  AND u1.posts < u2.posts
+ORDER BY
+u1.user_id;
+
+---Segundo Mayor y Menor
+(SELECT city_name, population
+FROM cities
+ORDER BY population desc
+LIMIT 1 OFFSET 1)
+UNION
+(SELECT city_name, population
+FROM cities
+ORDER BY population asc
+LIMIT 1 OFFSET 1)
+order by city_name desc
+
+
+
+
+
 
