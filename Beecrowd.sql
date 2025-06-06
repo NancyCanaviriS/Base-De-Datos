d8d650295316b1b29b7295935360604f8c62cd86
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-06 17:29:21 -0400

    nivel6

--- Beecrowd.sql
+++ Beecrowd.sql
@@ -410,7 +410,34 @@ INNER JOIN categories
 GROUP BY categories.id
 ORDER BY categories.name;
 
----
+---*****************NIVEL 6****************
+---Lawyers
+WITH average_customers AS (
+	SELECT 'Average' as name,
+		CAST(AVG(customers_number) AS integer) as customers_number,
+		2 AS order_filter
+	FROM lawyers	
+)
+SELECT name,
+	customers_number
+FROM (
+	SELECT 
+		name,
+		customers_number,
+		1 AS order_filter
+	FROM lawyers
+	WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers)
+		OR customers_number = (SELECT MIN(customers_number) FROM lawyers)
+	UNION ALL (TABLE average_customers) 
+) AS lawyers_customers
+ORDER BY order_filter, customers_number DESC;
+
+---payday
+SELECT
+    name,
+    CAST(EXTRACT(DAY from payday) AS integer) AS day
+FROM loan;
+
 
 
 
